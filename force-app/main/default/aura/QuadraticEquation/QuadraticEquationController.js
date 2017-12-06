566713f8e0e49ad208dd96538139f5bfba25c996
Mike <misha-radchenko@yandex.ru>
2017-12-06 08:18:53 +0300

    Create Solution component for displaying results

--- force-app/main/default/aura/QuadraticEquation/QuadraticEquationController.js
+++ force-app/main/default/aura/QuadraticEquation/QuadraticEquationController.js
@@ -1,5 +1,6 @@
 ({
     doInit: function (component, event, helper) {
+        component.set("v.solution", "Nothing to solve yet");
         component.set("v.equationParams", []);
         component.set("v.isFormValid", false);
     },
